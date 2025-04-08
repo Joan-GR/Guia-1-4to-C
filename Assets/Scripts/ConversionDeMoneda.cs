8049f348375012935c260988dc685093f4ef9de4
Joan-GR <joanguerson@gmail.com>
2025-04-08 09:05:14 -0300

    Ejercicio If 1

---
+++ Assets/Scripts/ConversionDeMoneda.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionDeMoneda : MonoBehaviour
+{
+    public float montoAConvertir;
+    public string codigoMoneda;
+    float montoFinal;
+    float cotizacionMonedaElegida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (montoAConvertir < 1000)
+        {
+            Debug.Log("El monto mínimo es $1000");
+            return;
+        }
+
+        if (codigoMoneda == "D")
+        { 
+            cotizacionMonedaElegida = 1075.20f;
+            
+        }
+        else if (codigoMoneda == "R")
+        {
+            cotizacionMonedaElegida = 181.78f;
+            
+        }
+        else if (codigoMoneda == "E")
+        {
+            cotizacionMonedaElegida = 1176.43f;
+            
+        }
+        else
+        {
+            Debug.Log("Opcion de moneda extranjera no valida");
+            return;
+        }
+
+        montoFinal = montoAConvertir / cotizacionMonedaElegida;
+
+        Debug.Log(montoFinal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
